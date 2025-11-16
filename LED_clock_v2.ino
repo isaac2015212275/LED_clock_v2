7de9fd00eee8967170f6701acae01de9780b3c62
isaac2015212275 <isaac2015212275@gmail.com>
2025-11-16 18:06:47 +0800

    Update LED_clock_v2.ino

--- LED_clock_v2.ino
+++ LED_clock_v2.ino
@@ -3,9 +3,9 @@
 
 int t_hour, t_min, t_sec, millisNow, millisPrev;
 int minCounterPrev, hourCounterPrev;
-bool canAdjMin, canAdjHour; 
+bool canAdjMin, canAdjHour, dotisOn; 
 TM1637Display tm1637(3,2);
-uint8_t data[] = { 0xFf, 0xff, 0xff, 0xff };
+uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
 uint8_t nums[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x6f };
 
 void setup() {
@@ -15,6 +15,7 @@ void setup() {
   t_hour = 0;
   t_min = 0;
   t_sec = 0;
+  dotisOn = false;
   tm1637.setBrightness(0xff);
   setTime();
   millisNow = millis();
@@ -25,27 +26,34 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //tm1637.setSegments(data);
+  tm1637.setSegments(data);
+  //tm1637.dot = 1;
   if(digitalRead(4)==HIGH&&canAdjMin)
     addTime(false);
   else if(digitalRead(5)==LOW&&canAdjHour)
     addTime(true);
   millisNow = millis();
-  if(millisNow - minCounterPrev >= 500)
+  if(millisNow - minCounterPrev >= 300)
     canAdjMin = true;
-  if(millisNow - hourCounterPrev >= 500)
+  if(millisNow - hourCounterPrev >= 300)
     canAdjHour = true;
   if(millisNow-millisPrev>=1000)
     tickTime();
+  else if(millisNow-millisPrev>=600)
+    dotisOn = true;
   setTime();
+  delay(2);
 }
 
 void setTime() {
-  int digit[] = { t_hour/10, t_hour%10, t_min/10, t_min%10};
-  for(int i=0; i<4 ; i++) {
-    data[i] = nums[digit[i]];
-    tm1637.setSegments(data);
+  int digit[] = {t_hour/10, t_hour%10, t_min/10, t_min%10};
+  for(int i=0; i<4; i++) {
+    if(dotisOn)
+      data[i] = nums[digit[i]] + 0x80;
+    else
+      data[i] = nums[digit[i]];
   }
+  tm1637.setSegments(data);
 }
 
 void addTime(bool isHour) {
@@ -66,6 +74,7 @@ void addTime(bool isHour) {
 }
 
 void tickTime() {
+  dotisOn = false;
   millisPrev = millisNow;
   t_sec++;
   t_min+=t_sec/60;
